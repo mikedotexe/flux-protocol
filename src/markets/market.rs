4649a8af79b2e40dd5f290888b413861382e6f5e
Jasper De gooijer <jasperdegooijer@gmail.com>
2020-04-01 19:32:27 +0200

    added block timestamp as creation time and check that end_time < blocktimestamo when placing an order

--- src/markets/market.rs
+++ src/markets/market.rs
@@ -1,7 +1,7 @@
 use std::string::String;
 use std::collections::BTreeMap;
 use std::collections::HashMap;
-use near_bindgen::{near_bindgen};
+use near_bindgen::{near_bindgen, env};
 use borsh::{BorshDeserialize, BorshSerialize};
 use serde::{Deserialize, Serialize};
 
@@ -24,7 +24,7 @@ pub struct Market {
 	pub outcome_tags: Vec<String>,
 	pub categories: Vec<String>,
 	pub last_price_for_outcomes: HashMap<u64, u128>,
-	// creation_time: u128
+	pub creation_time: u64,
 	pub end_time: u64,
 	pub orderbooks: BTreeMap<u64, orderbook::Orderbook>,
 	pub winning_outcome: Option<u64>,
@@ -50,6 +50,7 @@ impl Market {
 			outcome_tags,
 			categories,
 			last_price_for_outcomes: HashMap::new(),
+			creation_time: env::block_timestamp(),
 			end_time,
 			orderbooks: empty_orderbooks,
 			winning_outcome: None,
@@ -62,6 +63,7 @@ impl Market {
 		assert!(spend > 0);
 		assert!(price_per_share > 0 && price_per_share < 100);
 		assert_eq!(self.resoluted, false);
+		assert!(env::block_timestamp() < self.end_time);
 		let (spend_filled, shares_filled) = self.fill_matches(outcome, spend, price_per_share, 0);
 		let total_spend = spend - spend_filled;
 		self.liquidity += spend;
