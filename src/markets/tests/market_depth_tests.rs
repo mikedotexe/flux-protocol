5c2e651e63ea59788f5434efb20c4ed57c020714
Jasper De gooijer <jasperdegooijer@gmail.com>
2020-04-02 13:35:18 +0200

    simplified initial test case, gradually doing more complicated test cases, also implemented the liquidity_getter

--- src/markets/tests/market_depth_tests.rs
+++ src/markets/tests/market_depth_tests.rs
@@ -14,22 +14,11 @@ fn test_valid_binary_market_depth() {
 	contract.place_order(0, 1, 1000, 20);
 	contract.place_order(0, 1, 1000, 10);
 
-    let market_0 = contract.active_markets.get(&0).unwrap();
-    let orderbook_0 = market_0.orderbooks.get(&0).unwrap();
-    let mut yes_market_price = contract.get_market_price(0, 0);
-    let depth_0 = orderbook_0.get_liquidity(1000, 75);
+	let depth_0 = contract.get_liquidity(0, 0, 1000, 75); // Returns (max_price_payed, number of shares that can be purached, max_spend at max_price)
+	let depth_1 = contract.get_liquidity(0, 1, 1000, 11);
 
-	let market_1 = contract.active_markets.get(&0).unwrap();
-    let orderbook_1 = market_1.orderbooks.get(&1).unwrap();
-    let depth_1 = orderbook_1.get_liquidity(1000, 11);
-
-    let market_2 = contract.active_markets.get(&0).unwrap();
-    let orderbook_2 = market_2.orderbooks.get(&2).unwrap();
-    let depth_2 = orderbook_2.get_liquidity(5000, 24);
-
-    assert_eq!(depth_0, (60 , 16, 960));
-	assert_eq!(depth_1, (10, 100, 1000));
-	assert_eq!(depth_2, (23, 133, 2969));
+    assert_eq!(depth_0, (0 , 0, 0));
+	assert_eq!(depth_1, (0, 0, 0));
 }
 
 
