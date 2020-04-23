f990a518413cabf5aa684117355faf301ad1f80d
Jasper De gooijer <jasperdegooijer@gmail.com>
2020-04-23 15:42:01 +0200

    fixed final merge errors

--- src/markets/tests/market_depth_tests.rs
+++ src/markets/tests/market_depth_tests.rs
@@ -1,35 +1,20 @@
 use super::*;
 
-<<<<<<< HEAD
-#[test]
-fn test_liquidity_for_price() {
-	testing_env!(get_context(carol(), current_block_timestamp()));	
-	let mut contract = Markets::default();
-	contract.claim_fdai();
-	contract.create_market("Hi!".to_string(), empty_string(), 2, outcome_tags(0), categories(), market_creation_timestamp());
-=======
 // #[test]
 // fn test_liquidity_for_price() {
 // 	testing_env!(get_context(carol(), current_block_timestamp()));
 // 	let mut contract = Markets::default();
 // 	contract.claim_fdai();
 // 	contract.create_market("Hi!".to_string(), empty_string(), 2, outcome_tags(0), categories(), market_end_timestamp());
->>>>>>> ce15ee57b6511b1f80e7545cc9b7cf1cf5e6e0f5
 
 //	contract.place_order(0, 0, 6000, 50); // canceled before fill
 //	contract.place_order(0, 0, 6000, 50); // canceled before fill
 //	contract.place_order(0, 0, 6000, 20);
 //	contract.place_order(0, 0, 8000, 20); // canceled before fill
 
-<<<<<<< HEAD
-	let liquidity_60 = contract.get_liquidity(0, 0, 60);
-	let liquidity_50 = contract.get_liquidity(0, 0, 50);
-	let liquidity_20 = contract.get_liquidity(0, 0, 20);
-=======
 // 	let liquidity_60 = contract.get_liquidity_for_price(0, 0, 60);
 // 	let liquidity_50 = contract.get_liquidity_for_price(0, 0, 50);
 // 	let liquidity_20 = contract.get_liquidity_for_price(0, 0, 20);
->>>>>>> ce15ee57b6511b1f80e7545cc9b7cf1cf5e6e0f5
 
 // 	assert_eq!(liquidity_60, 0);
 // 	assert_eq!(liquidity_50, 12000);
@@ -39,26 +24,16 @@ fn test_liquidity_for_price() {
 //	contract.cancel_order(0,0,1);
 //	contract.cancel_order(0,0,3);
 
-<<<<<<< HEAD
-	let liquidity_50 = contract.get_liquidity(0, 0, 50);
-	let liquidity_20 = contract.get_liquidity(0, 0, 20);
-=======
 //	let liquidity_50 = contract.get_liquidity_for_price(0, 0, 50);
 //	let liquidity_20 = contract.get_liquidity_for_price(0, 0, 20);
->>>>>>> ce15ee57b6511b1f80e7545cc9b7cf1cf5e6e0f5
 
 //	assert_eq!(liquidity_50, 0);
 //	assert_eq!(liquidity_20, 6000 / 20);
 
 //	contract.place_order(0, 1, 8000, 80);
 
-<<<<<<< HEAD
-	let liquidity_20 = contract.get_liquidity(0, 0, 20);
-	let liquidity_80 = contract.get_liquidity(0, 1, 80);
-=======
 //	let liquidity_20 = contract.get_liquidity_for_price(0, 0, 20);
 //	let liquidity_80 = contract.get_liquidity_for_price(0, 1, 80);
->>>>>>> ce15ee57b6511b1f80e7545cc9b7cf1cf5e6e0f5
 
 //	assert_eq!(liquidity_20, 4000 / 20);
 //	assert_eq!(liquidity_80, 0);
--- src/markets/tests/market_resolution_tests.rs
+++ src/markets/tests/market_resolution_tests.rs
@@ -54,11 +54,7 @@ fn test_valid_market_payout_calc() {
 	testing_env!(get_context(carol(), current_block_timestamp()));
 	let mut contract = Markets::default();
 	contract.claim_fdai();
-<<<<<<< HEAD
-	contract.create_market("Hi!".to_string(), empty_string(), 4, outcome_tags(4), categories(), market_creation_timestamp());
-=======
 	contract.create_market("Hi!".to_string(), empty_string(), 4, outcome_tags(4), categories(), market_end_timestamp(), 0, 0, "test".to_string());
->>>>>>> ce15ee57b6511b1f80e7545cc9b7cf1cf5e6e0f5
 
 	contract.place_order(0, 0, 7000, 70);
 
