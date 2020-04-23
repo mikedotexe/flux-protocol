ccdbc78fc73e192174931fccc507722f3f9da7ce
Jasper De gooijer <jasperdegooijer@gmail.com>
2020-04-23 17:07:53 +0200

    fixed market depth test to take into account that get_liquidity returns amount of shares fillable

--- src/markets/tests/market_depth_tests.rs
+++ src/markets/tests/market_depth_tests.rs
@@ -1,43 +1,43 @@
 use super::*;
 
-// #[test]
-// fn test_liquidity_for_price() {
-// 	testing_env!(get_context(carol(), current_block_timestamp()));
-// 	let mut contract = Markets::default();
-// 	contract.claim_fdai();
-// 	contract.create_market("Hi!".to_string(), empty_string(), 2, outcome_tags(0), categories(), market_end_timestamp());
+#[test]
+fn test_liquidity_for_price() {
+	testing_env!(get_context(carol(), current_block_timestamp()));
+	let mut contract = Markets::default();
+	contract.claim_fdai();
+	contract.create_market("Hi!".to_string(), empty_string(), 2, outcome_tags(0), categories(), market_end_timestamp(), 0, 0, "test".to_string());
 
-//	contract.place_order(0, 0, 6000, 50); // canceled before fill
-//	contract.place_order(0, 0, 6000, 50); // canceled before fill
-//	contract.place_order(0, 0, 6000, 20);
-//	contract.place_order(0, 0, 8000, 20); // canceled before fill
+	contract.place_order(0, 0, 6000, 50);
+	contract.place_order(0, 0, 6000, 50); 
+	contract.place_order(0, 0, 6000, 20);
+	contract.place_order(0, 0, 8000, 20); 
 
-// 	let liquidity_60 = contract.get_liquidity(0, 0, 60);
-// 	let liquidity_50 = contract.get_liquidity(0, 0, 50);
-// 	let liquidity_20 = contract.get_liquidity(0, 0, 20);
+	let liquidity_60 = contract.get_liquidity(0, 0, 60);
+	let liquidity_50 = contract.get_liquidity(0, 0, 50);
+	let liquidity_20 = contract.get_liquidity(0, 0, 20);
 
-// 	assert_eq!(liquidity_60, 0);
-// 	assert_eq!(liquidity_50, 12000);
-// 	assert_eq!(liquidity_20, 14000);
+	assert_eq!(liquidity_60, 0);
+	assert_eq!(liquidity_50, 12000 / 50);
+	assert_eq!(liquidity_20, 14000 / 20);
 
-//	contract.cancel_order(0,0,0);
-//	contract.cancel_order(0,0,1);
-//	contract.cancel_order(0,0,3);
+	contract.cancel_order(0,0,0);
+	contract.cancel_order(0,0,1);
+	contract.cancel_order(0,0,3);
 
-//	let liquidity_50 = contract.get_liquidity(0, 0, 50);
-//	let liquidity_20 = contract.get_liquidity(0, 0, 20);
+	let liquidity_50 = contract.get_liquidity(0, 0, 50);
+	let liquidity_20 = contract.get_liquidity(0, 0, 20);
 
-//	assert_eq!(liquidity_50, 0);
-//	assert_eq!(liquidity_20, 6000 / 20);
+	assert_eq!(liquidity_50, 0);
+	assert_eq!(liquidity_20, 6000 / 20);
 
-//	contract.place_order(0, 1, 8000, 80);
+	contract.place_order(0, 1, 8000, 80);
 
-//	let liquidity_20 = contract.get_liquidity(0, 0, 20);
-//	let liquidity_80 = contract.get_liquidity(0, 1, 80);
+	let liquidity_20 = contract.get_liquidity(0, 0, 20);
+	let liquidity_80 = contract.get_liquidity(0, 1, 80);
 
-//	assert_eq!(liquidity_20, 4000 / 20);
-//	assert_eq!(liquidity_80, 0);
-//}
+	assert_eq!(liquidity_20, 4000 / 20);
+	assert_eq!(liquidity_80, 0);
+}
 
 #[test]
 fn test_valid_binary_market_depth() {
